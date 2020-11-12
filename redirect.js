c2ec9ca96f3e05c3605df9235abc3ef54a676661
Ghaly Aditya <ghaly@kata.ai>
2020-11-12 13:08:39 +0700

    first commit

--- redirect.js
+++ redirect.js
@@ -9,21 +9,21 @@ setTimeout(() => {
     }
 }, 2000);
 
-window.addEventListener('message', function (eventData) {
-    try {
-        if (JSON.parse(eventData.data)) {
-            let event = JSON.parse(eventData.data);
+// window.addEventListener('message', function (eventData) {
+//     try {
+//         if (JSON.parse(eventData.data)) {
+//             let event = JSON.parse(eventData.data);
 
-            if (event.event_code === "custom-event" && event.data && event.data.code === "thank_you_page") {
-                var newWindow = window.open(event.data.data, "_self");
-                return;
-            } else {
-                return;
-            }
-        }
-        return;
-    } catch (error) {
-        console.log(error, "error")
-        return;
-    }
-}, false);
+//             if (event.event_code === "custom-event" && event.data && event.data.code === "thank_you_page") {
+//                 var newWindow = window.open(event.data.data, "_self");
+//                 return;
+//             } else {
+//                 return;
+//             }
+//         }
+//         return;
+//     } catch (error) {
+//         console.log(error, "error")
+//         return;
+//     }
+// }, false);
