d08a744237d49a58340cf9bde5de3c8a99e2556f
Ghaly Aditya <ghaly@kata.ai>
2020-11-12 13:10:14 +0700

    first commit

--- redirect.js
+++ redirect.js
@@ -3,9 +3,9 @@ setTimeout(() => {
     doc = doc.contentDocument || doc.contentWindow;
     let r = doc.lastChild.getElementsByClassName('title');
     if (r.length) {
-        let payload = JSON.parse(window.ymConfig.payload);
+        // let payload = JSON.parse(window.ymConfig.payload);
         r = r[0];
-        r.innerText = payload.university;
+        r.innerText = window.ymConfig.payload.university;
     }
 }, 2000);
 
